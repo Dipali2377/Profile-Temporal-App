25dd3d77e172de28bb0a86c40421355b8ab211f5
Dipali2377 <dipalim680@gmail.com>
2025-06-12 23:29:27 +0530

    working application completed

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -12,4 +12,5 @@ userRouter.get("/", getAllUsers);
 userRouter.post("/login", loginUser);
 userRouter.get("/:email", getUserByEmail);
 userRouter.put("/:email", updateUserByEmail);
+
 export default userRouter;
